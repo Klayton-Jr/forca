48dd93d55c90bc60b6e45c5d8fa5cedd01872b16
JoelVelloso <joel@se7esistemas.com.br>
2020-12-03 16:50:00 -0300

    Commit Joel

--- server/src/main/java/br/com/view/PainelNovaSala.java
+++ server/src/main/java/br/com/view/PainelNovaSala.java
@@ -8,7 +8,11 @@ import javafx.scene.layout.AnchorPane;
 public class PainelNovaSala extends Painel {
 
     private Button btnVoltar;
+    private Button btnCriarSala;
     private TextField edtNomeSala;
+    private IntTextField edtQuantidadeMaximaJogadores;
+    private IntTextField edtNumeroRodadas;
+    private IntTextField edtTempoResponderCadaLetra;
 
     public PainelNovaSala() {
         inicializar();
@@ -20,18 +24,73 @@ public class PainelNovaSala extends Painel {
         AnchorPane.setLeftAnchor(btnVoltar, 10.0);
         getChildren().add(btnVoltar);
 
+        btnCriarSala = new Button("Criar");
+        AnchorPane.setTopAnchor(btnCriarSala, 10.0);
+        AnchorPane.setRightAnchor(btnCriarSala, 10.0);
+        getChildren().add(btnCriarSala);
+
+        AnchorPane painelConfiguracoes = new AnchorPane();
+        painelConfiguracoes.setStyle("-fx-background-color: #FFFFFF");
+        AnchorPane.setTopAnchor(painelConfiguracoes, 50.0);
+        AnchorPane.setLeftAnchor(painelConfiguracoes, 10.0);
+        AnchorPane.setRightAnchor(painelConfiguracoes, 10.0);
+        AnchorPane.setBottomAnchor(painelConfiguracoes, 10.0);
+        getChildren().add(painelConfiguracoes);
+
         Label rtlNomeSala = new Label("Nome da Sala:");
-        rtlNomeSala.setPrefWidth(100);
+        rtlNomeSala.setPrefWidth(200);
         rtlNomeSala.setStyle("-fx-text-alignment: right; -fx-alignment: center-right");
         AnchorPane.setTopAnchor(rtlNomeSala, 43.0);
         AnchorPane.setLeftAnchor(rtlNomeSala, 10.0);
-        getChildren().add(rtlNomeSala);
+        painelConfiguracoes.getChildren().add(rtlNomeSala);
 
         edtNomeSala = new TextField();
         AnchorPane.setTopAnchor(edtNomeSala, 40.0);
-        AnchorPane.setLeftAnchor(edtNomeSala, 115.0);
+        AnchorPane.setLeftAnchor(edtNomeSala, 215.0);
         AnchorPane.setRightAnchor(edtNomeSala, 10.0);
-        getChildren().add(edtNomeSala);
+        painelConfiguracoes.getChildren().add(edtNomeSala);
+
+        Label rtlQuantidadeMaximaJogadores = new Label("Quantidade Máxima Jogadores:");
+        rtlQuantidadeMaximaJogadores.setPrefWidth(200);
+        rtlQuantidadeMaximaJogadores.setStyle("-fx-text-alignment: right; -fx-alignment: center-right");
+        AnchorPane.setTopAnchor(rtlQuantidadeMaximaJogadores, 73.0);
+        AnchorPane.setLeftAnchor(rtlQuantidadeMaximaJogadores, 10.0);
+        painelConfiguracoes.getChildren().add(rtlQuantidadeMaximaJogadores);
+
+        edtQuantidadeMaximaJogadores = new IntTextField();
+        edtQuantidadeMaximaJogadores.setValue(4);
+        AnchorPane.setTopAnchor(edtQuantidadeMaximaJogadores, 70.0);
+        AnchorPane.setLeftAnchor(edtQuantidadeMaximaJogadores, 215.0);
+        AnchorPane.setRightAnchor(edtQuantidadeMaximaJogadores, 10.0);
+        painelConfiguracoes.getChildren().add(edtQuantidadeMaximaJogadores);
+
+        Label rtlNumeroRodadas = new Label("Número de Rodadas:");
+        rtlNumeroRodadas.setPrefWidth(200);
+        rtlNumeroRodadas.setStyle("-fx-text-alignment: right; -fx-alignment: center-right");
+        AnchorPane.setTopAnchor(rtlNumeroRodadas, 103.0);
+        AnchorPane.setLeftAnchor(rtlNumeroRodadas, 10.0);
+        painelConfiguracoes.getChildren().add(rtlNumeroRodadas);
+
+        edtNumeroRodadas = new IntTextField();
+        edtNumeroRodadas.setValue(10);
+        AnchorPane.setTopAnchor(edtNumeroRodadas, 100.0);
+        AnchorPane.setLeftAnchor(edtNumeroRodadas, 215.0);
+        AnchorPane.setRightAnchor(edtNumeroRodadas, 10.0);
+        painelConfiguracoes.getChildren().add(edtNumeroRodadas);
+
+        Label rtlTempoResponderCadaLetra = new Label("Tempo para responder cada letra (s):");
+        rtlTempoResponderCadaLetra.setPrefWidth(200);
+        rtlTempoResponderCadaLetra.setStyle("-fx-text-alignment: right; -fx-alignment: center-right");
+        AnchorPane.setTopAnchor(rtlTempoResponderCadaLetra, 133.0);
+        AnchorPane.setLeftAnchor(rtlTempoResponderCadaLetra, 10.0);
+        painelConfiguracoes.getChildren().add(rtlTempoResponderCadaLetra);
+
+        edtTempoResponderCadaLetra = new IntTextField();
+        edtTempoResponderCadaLetra.setValue(15);
+        AnchorPane.setTopAnchor(edtTempoResponderCadaLetra, 130.0);
+        AnchorPane.setLeftAnchor(edtTempoResponderCadaLetra, 215.0);
+        AnchorPane.setRightAnchor(edtTempoResponderCadaLetra, 10.0);
+        painelConfiguracoes.getChildren().add(edtTempoResponderCadaLetra);
     }
 
     public Button getBtnVoltar() {
@@ -41,5 +100,8 @@ public class PainelNovaSala extends Painel {
     @Override
     public void clear() {
         edtNomeSala.clear();
+        edtQuantidadeMaximaJogadores.setValue(4);
+        edtNumeroRodadas.setValue(10);
+        edtTempoResponderCadaLetra.setValue(15);
     }
 }
