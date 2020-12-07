136fd12e1f770cfdf7edb75ce3da014ff0ac975f
Klayton Junior <klaytonjunior@hotmail.com>
2020-12-07 09:06:41 -0300

    Add files via upload
    refazendo a parte do servidor

---
+++ teste forca 2.0/forca/src/main/java/br/com/forca/model/Greeting.java
@@ -0,0 +1,17 @@
+package br.com.forca.model;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
