0623da5df1356ce738968c4d5d89dd3dab48fa64
Klayton Jr <klaytonjunior@hotmail.com>
2020-12-07 15:42:44 -0300

    reorganizando arquivos

---
+++ server/src/main/java/br/com/forca/model/ChatMessage.java
@@ -0,0 +1,28 @@
+package br.com.forca.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+public class ChatMessage {
+
+    @Getter @Setter
+    private String content;
+
+    @Getter @Setter
+    private String sender;
+
+    @Getter @Setter
+    private MessageType type;
+
+    @Getter @Setter
+    private String time;
+
+    public enum MessageType {
+        CHAT, CONNECT, DISCONNECT
+    }
+
+
+
+}
